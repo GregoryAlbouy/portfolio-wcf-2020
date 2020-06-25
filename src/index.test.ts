520a723ba023a52af376f1d823b6c0546da6b99c
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-06-25 19:57:41 +0200

    minor cleanup

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,13 +4,7 @@
 
 import './index.scss'
 import TestComponent from './tests/components/TestComponent/TestComponent'
-import MiniChat from './tests/components/MiniChat/MiniChat'
-// import MiniChat from './components/MiniChat/MiniChat'
 // import { loadProjectData } from './shared/utils'
-import IO from './tests/IO'
 
 // loadProjectData()
 document.body.appendChild(new TestComponent())
-document.body.appendChild(new MiniChat())
-// IO()
-// document.body.appendChild(new MiniChat())

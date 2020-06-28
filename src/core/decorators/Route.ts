e102e3e7c6e2538b7036e296b314098e4aa5674f
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-06-28 17:29:29 +0200

    create routing base

--- src/core/decorators/Route.ts
+++ src/core/decorators/Route.ts
@@ -1,3 +1,6 @@
+import type { RouteComponent } from '../routing/Router'
+import { Router } from '../routing'
+
 interface RouteOptions {
     beforeAppend?: Function
     afterAppend?: Function
@@ -9,7 +12,7 @@ interface RouteOptions {
  * Component class decorator: automatically do Route stuff
  */
 export default function Route(path: string, options: RouteOptions = {}) {
-    return (target: Function) => {
-        console.log(`My name is ${target.name}, I'm a route and my @Route decorator works well.`)
+    return (target: RouteComponent) => {
+        Router.setRoute(path, target)
     }
 }
--- src/core/routing/Router.ts
+++ src/core/routing/Router.ts
@@ -1,47 +1,73 @@
-type RouteComponent = HTMLElement
-type RouteMap = { [path: string]: typeof HTMLElement }
-type RouteObject = { [path: string]: typeof HTMLElement }
-type RouteAnimation = (prev: RouteComponent, next: RouteComponent ) => Promise<void>
-type RouterOptions = {
-    animate?: RouteAnimation,
-    defaultPath?: string,
-    listen?: boolean
-}
+export type RouteComponent = HTMLElement | typeof HTMLElement
+type RouteMap = Map<string, RouteComponent>
+// type RouteObject = { [path: string]: typeof HTMLElement }
+// type RouteAnimation = (prev: RouteComponent, next: RouteComponent ) => Promise<void>
+// type RouterOptions = {
+//     animate?: RouteAnimation,
+//     defaultPath?: string,
+//     listen?: boolean
+// }
 
 export default class Router {
-    routes: RouteMap
-    current: RouteComponent
-    animate?: RouteAnimation
+    static routes: RouteMap = new Map()
+    // currentPath: string
+    // currentPage: RouteComponent
 
-    constructor(routes: RouteMap | RouteObject, options: RouterOptions = {}) {
-        const { defaultPath, animate, listen } = options
+    constructor() {
+        // this.routes = new Map()
+    }
 
-        this.routes = routes
-        this.current = new this.routes['/']()
-        if (animate) this.animate = animate
-        this.init()
-        this.listen()
+    static setRoute(path: string, component: RouteComponent) {
+        Router.routes.set(path, component)
+        console.log(Router.routes)
     }
 
     listen() {
-        window.addEventListener('popstate', (event) => console.log(event))
-    }
-    init() {
-        document.body.appendChild(this.current)
+
     }
 
-    async goto(path: string, animate?: RouteAnimation) {
-        const prev = this.current
-        const next = new this.routes[path]()
-        this.current = next
-
-        window.history.pushState(null, '', path)
-        prev.insertAdjacentElement('afterend', next)
-        if (animate) await animate(prev, next)
-        prev.remove()
+    to() {
 
     }
 }
+
+// move to index.ts?
+export const router = new Router()
+
+// export default class Router {
+//     routes: RouteMap
+//     current: RouteComponent
+//     animate?: RouteAnimation
+
+//     constructor(routes: RouteMap | RouteObject, options: RouterOptions = {}) {
+//         const { defaultPath, animate, listen } = options
+
+//         this.routes = routes
+//         this.current = new this.routes['/']()
+//         if (animate) this.animate = animate
+//         this.init()
+//         this.listen()
+//     }
+
+//     listen() {
+//         window.addEventListener('popstate', (event) => console.log(event))
+//     }
+//     init() {
+//         document.body.appendChild(this.current)
+//     }
+
+//     async goto(path: string, animate?: RouteAnimation) {
+//         const prev = this.current
+//         const next = new this.routes[path]()
+//         this.current = next
+
+//         window.history.pushState(null, '', path)
+//         prev.insertAdjacentElement('afterend', next)
+//         if (animate) await animate(prev, next)
+//         prev.remove()
+
+//     }
+// }
 
 // const animationCallback = (prev: any, next: any) => {
 //     return console.log('animationCallback() called')

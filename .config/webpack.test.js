f949d570039deb4f41bc3aa0967a6c2e00739838
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-07-06 11:53:30 +0200

    display test config info on start

--- .config/webpack.test.js
+++ .config/webpack.test.js
@@ -3,6 +3,14 @@ const webpackMerge = require('webpack-merge')
 const devConfig = require('./webpack.dev.js')
 
 const PORT = 9998
+const MODE = 'development'
+const API_BASE_URL =  process.env.LOCAL_API_BASE_URL
+
+console.table({
+    PORT,
+    MODE,
+    API_BASE_URL
+})
 
 module.exports = webpackMerge(devConfig, {
     entry: path.resolve(__dirname, '../tests/index.test.ts'),

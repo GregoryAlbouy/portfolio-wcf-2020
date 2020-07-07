62cc75744882e6916a42f8e2a911e30ebeb2404b
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-07-07 15:40:40 +0200

    remove routing test experimentations

--- tests/components/test-components.ts
+++ tests/components/test-components.ts
@@ -1,17 +1,5 @@
 import TestComponent from './TestComponent/TestComponent'
-import RouteContact from './RouteContact/RouteContact'
-import RouteProjects from './RouteProjects/RouteProjects'
-import RouteHome from './RouteHome/RouteHome'
-import Route404 from './Route404/Route404'
-import NavComponent from './NavComponent/NavComponent'
-import AppRootTest from './AppRootTest/AppRootTest'
 
 export {
     TestComponent,
-    RouteContact,
-    RouteProjects,
-    RouteHome,
-    NavComponent,
-    AppRootTest,
-    Route404
 }
